52c1afcf5bff4884a31efae464a1b6798beb5399
maheshmuttinti <maheshmuttinti@betalectic.com>
2023-01-08 15:43:01 +0530

    chore: setup eslint and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es2021: true,
+		"react-native/react-native": true,
+		"jest/globals": true,
+	},
+	extends: [
+		"plugin:react/recommended",
+		"prettier",
+		"eslint:recommended",
+		"plugin:jest/recommended",
+		// "@react-native-community/eslint-config",
+	],
+
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+
+	plugins: ["react", "react-native"],
+	ignorePatterns: ["!.*", "dist", "node_modules"],
+	rules: {
+		indent: [
+			"error",
+			"tab",
+			{
+				SwitchCase: 1,
+				ignoredNodes: ["ConditionalExpression"],
+			},
+		],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"no-console": ["error"],
+		"no-unused-vars": ["error", { vars: "all" }],
+	},
+
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
