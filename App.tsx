01e57ea65d1e9f33df639a8f0e0d2ae5c4e183ac
maheshmuttinti <maheshmuttinti@betalectic.com>
2023-01-08 15:08:39 +0530

    chore: change the App.js to App.tsx

---
+++ App.tsx
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {Heading} from './packages/components';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Heading
+          wrapperStyle={{backgroundColor: 'red'}}
+          title={'Heading Component'}
+        />
+
+        <View style={{}}></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
